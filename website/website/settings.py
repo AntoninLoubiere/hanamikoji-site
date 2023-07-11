b9efe444ed2d8c94d89683845579d17cf2d1c7b0
Triw121204 <pagezy.tristan@gmail.com>
2023-07-11 08:01:53 +0200

    Ajout css

--- website/website/settings.py
+++ website/website/settings.py
@@ -57,7 +57,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            BASE_DIR.joinpath('templates'),
+            BASE_DIR / 'templates',
         ],
         'APP_DIRS': True,
         'OPTIONS': {
