4e9dbab7f59dbfb4b371c29f9b1874d3370e9c78
Tristan Pagezy <61625831+Triw-12@users.noreply.github.com>
2023-08-23 10:47:45 +0200

    Update website/static/play/play.js
    Co-authored-by: Antonin Loubiere <40536532+AntoninLoubiere@users.noreply.github.com>

--- website/static/play/play.js
+++ website/static/play/play.js
@@ -246,7 +246,7 @@ function applyOnStatus(data) {
     }
 
     if (isLastStatus(data) || data.tour <= tour) return;
-    let audio = new Audio("bell-me.mp3");
+    let audio = new Audio("/static/play/bell-me.mp3");
     audio.play();
     setStatusMsg('user_turn');
     tour = data.tour;
